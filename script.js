fa27d8439a4ac77dd55d9e21ab65d582772caf76
Leo <leomettam@gmail.com>
2025-08-30 20:33:22 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('navbar-menu-active'); // Make sure to use the correct class name
+});
